b09a7f7960085f2fe2e4e82b855c32f14c3e30e1
nappylae-code <nappylae-code@users.noreply.github.com>
2026-04-09 15:24:17 +0900

    chore: remove console.error for cloud init failure in app.ts

--- app.ts
+++ app.ts
@@ -54,9 +54,7 @@ if (!globalObj.crypto.randomBytes) {
 App<IAppOption>({
   onLaunch() {
     // Initialize WeChat Cloud Base
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
-    } else {
+    if (wx.cloud) {
       wx.cloud.init({
         env: 'cloud1-3gh5mibgd5111425',
         traceUser: true
